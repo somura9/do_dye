87917d35aed1af2c9aaaede4ec2a674c13dd712f
somura9 <akathe917@gmail.com>
2022-12-19 20:50:26 +0900

    add: work,userモデルにバリデーションを追加

--- app/models/work.rb
+++ app/models/work.rb
@@ -1,3 +1,6 @@
 class Work < ApplicationRecord
+  validates :title,   presence: true, length: { maximum: 255 }
+  validates :summary,    presence: true, length: { maximum: 255 }
+
   belongs_to :user
 end
