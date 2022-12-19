ee43278084d8e7efa704a254660381af78cb4079
somura9 <akathe917@gmail.com>
2022-12-19 22:09:55 +0900

    fix: 他の人の作品を編集できてしまう問題を修正

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -1,5 +1,5 @@
 class WorksController < ApplicationController
-  before_action :set_work, only: %i[show edit update destroy]
+  before_action :set_current_user_work, only: %i[edit update destroy]
   skip_before_action :require_login, only: %i[new create show index]
 
   def new
@@ -22,6 +22,7 @@ class WorksController < ApplicationController
   end
 
   def show
+    @work = Work.find(params[:id])
   end
 
   def edit
@@ -42,8 +43,8 @@ class WorksController < ApplicationController
   end
 
   private
-  def set_work
-    @work = Work.find(params[:id])
+  def set_current_user_work
+    @work = current_user.works.find(params[:id])
   end
 
   def work_params
