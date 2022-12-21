b905108b01dd966e74be89c02d0178680efbd5f1
somura9 <akathe917@gmail.com>
2022-12-21 18:55:59 +0900

    add: flowbite, jquely のインポート設定

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,10 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+import 'flowbite'
+import jquery from "jquery"
+window.$ = jquery
+
+$(function() {
+  console.log("Hello Rails7!");
+})
