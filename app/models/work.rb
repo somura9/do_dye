ddd6cdf33f8b09061a80a1f3e7a70f1482cae9c3
somura9 <akathe917@gmail.com>
2022-12-22 14:07:59 +0900

    add: アップローダーのセットアップ

--- app/models/work.rb
+++ app/models/work.rb
@@ -1,4 +1,5 @@
 class Work < ApplicationRecord
+  mount_uploader :thumbnail, ThumbnailUploader
   validates :title,   presence: true, length: { maximum: 255 }
   validates :summary,    presence: true, length: { maximum: 255 }
 
---
+++ app/uploaders/avatar_uploader.rb
@@ -0,0 +1,33 @@
+class AvatarUploader < CarrierWave::Uploader::Base
+  # Include RMagick or MiniMagick support:
+  # include CarrierWave::RMagick
+  # include CarrierWave::MiniMagick
+
+  # Choose what kind of storage to use for this uploader:
+  storage :file
+  # storage :fog
+
+  def store_dir
+    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+  end
+
+  def default_url
+    'default_profile_400x400.png'
+  end
+
+  # Process files as they are uploaded:
+  # process scale: [200, 300]
+  #
+  # def scale(width, height)
+  #   # do something
+  # end
+
+  # Create different versions of your uploaded files:
+  # version :thumb do
+  #   process resize_to_fit: [50, 50]
+  # end
+
+  def extension_allowlist
+    %w(jpg jpeg gif png)
+  end
+end
