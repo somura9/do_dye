2691bd21bde0a71bbe6b4582821f31c6cff2b4ba
somura9 <akathe917@gmail.com>
2023-02-16 12:01:16 +0900

    fix: STDOUTを戻す

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,9 +83,9 @@ Rails.application.configure do
   # config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new "app-name")
 
   if ENV['RAILS_LOG_TO_STDOUT'].present?
-    logger           = ActiveSupport::Logger.new($stdout)
+    logger           = ActiveSupport::Logger.new(STDOUT)
     logger.formatter = config.log_formatter
-    config.logger    = ActiveSupport::TaggedLogging.new($stdout)
+    config.logger    = ActiveSupport::TaggedLogging.new(STDOUT)
   end
 
   # Do not dump schema after migrations.
